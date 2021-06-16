951986f5da2231d3e1f08a6400b5a76d759a253b
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-16 14:29:25 +0800

    Making a simple way to measure BMI
    【问题描述】定义一个身体质量指数类BMI，包含height(身高，m为单位)和weight(体重，kg为单位)两个成员变量。
    添加两个参数的构造方法，用两个参数分别初始化两个成员变量。
    方法getBMI()返回bmi值(体重除以身高的平方)。
    方法assessBMI(double bmi)返回参数bmi值对应的类型。
    方法show()调用getBMI()计算bmi，并使用bmi值做参数调用assessBMI()方法，输出身高、体重、bmi和类型。
    在TestBMI类的main()方法中输入身高和体重，用该数据创建对象，输出其身高、体重、bmi和类型。输入输出格式如样例所示，其中：红色文字为真正的程序输入，蓝色文字为输入提示。
    
    BMI2.png
    【样例输入1】
    1.68
    65
    【样例输出1】
    Please enter height(m):
    1.68
    Please enter weight(kg):
    65
    height 1.68m, weight 65.0kg, BMI is 23.030045351473923, Normal weight
    
    【样例输入2】
    1.7
    70
    【样例输出2】
    Please enter height(m):
    1.70
    Please enter weight(kg):
    70
    height 1.7m, weight 70.0kg, BMI is 24.221453287197235, Overweight
    
    【样例输入3】
    1.72
    50
    【样例输出3】
    Please enter height(m):
    1.72
    Please enter weight(kg):
    50
    height 1.72m, weight 50.0kg, BMI is 16.901027582477017, Low weight
    
    【样例输入4】
    1.6
    80
    【样例输出4】
    Please enter height(m):
    1.60
    Please enter weight(kg):
    80
    height 1.6m, weight 80.0kg, BMI is 31.25, Fat or Obesity

---
+++ TestBMI.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+class BMI{
+   private double height;
+   private double weight;
+   BMI(double height,double weight){
+       this.height=height;
+       this.weight=weight;
+   }
+   private double getBMI(){
+      double bmi=weight/(height*height);
+    //  System.out.println(bmi);
+      return bmi;
+
+   }
+    String assessBMI(double bmi){
+
+       if(bmi<18.5)
+           return "LOW WEIGHT";
+       else if(18.5<=bmi && bmi<=24)
+           return "Normal Weight";
+       else if(24<=bmi && bmi<28)
+       return "Overweight";
+       else
+       return "Fat or Obesity";
+
+    }
+
+
+    void show(){
+       double bmi = getBMI();
+        String TYPE=assessBMI(bmi);
+        System.out.printf("height =%fm, weight=%fkg , BMI is %f, %s",height,weight,bmi,TYPE);
+ }
+}
+public class TestBMI {
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);
+
+
+        System.out.println("Please enter height(m):");
+        double height=s.nextDouble();
+        System.out.println("Please enter weight(kg):");
+        double weight=s.nextDouble();
+        BMI b=new BMI(height,weight);
+        b.show();
+    }
+}
+
